7c95ed4e71d765ba95bc19bbb080374e3f552d86
Rahul Tiwari <tiwarirahul0809@gmail.com>
2023-09-09 03:35:57 +0530

    debugging

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ export PATH="$PATH:/home/ubuntu/.nvm/versions/node/v20.6.1/bin"
 
 cd /home/ubuntu/course-delevery-backend
 git pull origin main
-# yarn install
+yarn install
 pm2 stop cd
 pm2 start cd
 # pm2 start npm --name "cd" -- run "start"
