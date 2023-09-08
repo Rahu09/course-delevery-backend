f56fbf7ce7160a7b85083a2098d6371846d6464d
Rahul Tiwari <tiwarirahul0809@gmail.com>
2023-09-09 00:40:01 +0530

    scripy added

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export PATH="$PATH:/home/ubuntu/.nvm/versions/node/v20.6.0/bin"
+
+cd /home/ubuntu/course-delevery-backend
+git pull origin main
+yarn install
+yarn build
+pm2 stop cd
+pm2 start npm --name "cd" -- run "start"
