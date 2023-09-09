7d9a2ae623763b30aabe4d2ac0dd3b1237131603
Rahul Tiwari <tiwarirahul0809@gmail.com>
2023-09-09 23:04:10 +0530

    testing ci/cd

--- build/04-course-app-hard.js
+++ build/04-course-app-hard.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const express_1 = __importDefault(require("express"));
@@ -20,8 +22,7 @@ app.use((0, cookie_parser_1.default)());
 app.use("/api/admin", admin_1.default);
 app.use("/api/users", users_1.default);
 (0, data_1.connectDB)();
-// lets check deployment
 app.get("*", (req, res) => {
-    res.sendFile(path_1.default.join(__dirname, "dist/index.html"));
+  res.sendFile(path_1.default.join(__dirname, "dist/index.html"));
 });
 app.listen(3005, () => console.log(`Server running on port 3005`));
